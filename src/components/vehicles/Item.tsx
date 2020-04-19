6d1f43fe136b6c24d2dc1f0e5fe246d53aa869f4
Matias Chomicki <matias.chomicki@gmail.com>
2020-04-19 16:34:54 -0300

    [vehiculos] integrar los datos con el modelo y el model con el listado

--- src/components/vehicles/Item.tsx
+++ src/components/vehicles/Item.tsx
@@ -1,5 +1,23 @@
 import React from "react";
+import Vehicle from "models/Vehicle";
+import { Media } from "react-bootstrap";
 
-const VehicleItem: React.FC = () => <div />;
+const VehicleItem: React.FC<{ vehicle: Vehicle }> = ({ vehicle }) => (
+  <Media as="li">
+    <img
+      width={120}
+      height={120}
+      className="mr-3"
+      src="holder.js/64x64"
+      alt={vehicle.model}
+    />
+    <Media.Body>
+      <h5>
+        {vehicle.brand} {vehicle.model}
+      </h5>
+      <p>Una descripción.</p>
+    </Media.Body>
+  </Media>
+);
 
 export default VehicleItem;
--- src/components/vehicles/List.tsx
+++ src/components/vehicles/List.tsx
@@ -1,20 +1,34 @@
+import Item from "./Item";
 import React from "react";
+import Vehicle from "models/Vehicle";
 
 const VehicleList: React.FC = () => {
-  const vehicles = [
+  const data = [
     {
+      id: 1,
       brand: "Ford",
       model: "Mondeo",
       year: 2020,
+      price: 3000000,
     },
     {
+      id: 2,
       brand: "Volkswagen",
       model: "T-Cross",
       year: 2019,
+      price: 2500000,
     },
   ];
 
-  return <div />;
+  const vehicles = data.map((data) => new Vehicle(data));
+
+  return (
+    <ul className="list-unstyled">
+      {vehicles.map((vehicle) => (
+        <Item vehicle={vehicle} />
+      ))}
+    </ul>
+  );
 };
 
 export default VehicleList;
