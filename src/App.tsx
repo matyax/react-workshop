7e3dbbc6c5e24579f1fcb9d27c553123972f3f8f
Matias Chomicki <matias.chomicki@gmail.com>
2020-04-19 16:20:24 -0300

    [vehiculos] agregar listado e item
    Creamos dos componentes nuevos, los ubicamos en el àrbol del proyecto.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import Layout from "components/layout/Layout";
 import React from "react";
+import VehicleList from "components/vehicles/List";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
   return (
     <Layout>
-      <div>Futura app de React</div>
+      <VehicleList />
     </Layout>
   );
 }
---
+++ src/components/vehicles/List.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const VehicleList: React.FC = () => {
+  const vehicles = [
+    {
+      brand: "Ford",
+      model: "Mondeo",
+      year: 2020,
+    },
+    {
+      brand: "Volkswagen",
+      model: "T-Cross",
+      year: 2019,
+    },
+  ];
+
+  return <div />;
+};
+
+export default VehicleList;
