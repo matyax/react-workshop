c56258c34e74a0175488e34d2edb021af6177ee5
Matias Chomicki <matias.chomicki@gmail.com>
2020-04-19 16:49:46 -0300

    [app] actualizar el test, mockear lista de vehiculos

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,15 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
-  const { getByText } = render(<App />);
+jest.mock("components/vehicles/List", () => () => (
+  <div data-testid="vehicle-list" />
+));
+
+test("Renders without crashing", () => {
+  const { container, getByText, getByTestId } = render(<App />);
 
   expect(getByText("Autos Online")).toBeInTheDocument();
+  expect(getByTestId("vehicle-list")).toBeDefined();
+
+  expect(container).toMatchSnapshot();
 });
