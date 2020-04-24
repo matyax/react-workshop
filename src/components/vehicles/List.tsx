4dbd157a3daeeb4bda739c776fe842550870c14a
Matias Chomicki <matias.chomicki@vividcortex.com>
2020-04-24 20:35:46 -0300

    [vehiculos] agregar soporte para los distintos estados del componente
    Mostramos un spinner mientras se cargan los datos.
    
    Si no hay vehículos (por error o falta de stock), mostramos un mensaje.

--- src/components/vehicles/List.tsx
+++ src/components/vehicles/List.tsx
@@ -2,9 +2,10 @@ import Item from "./Item";
 import React, { useEffect, useState } from "react";
 import Vehicle from "models/Vehicle";
 import { get as getStock } from "services/stock";
+import { Spinner } from "react-bootstrap";
 
 const VehicleList: React.FC = () => {
-  const [vehicles, setVehicles] = useState<Vehicle[]>([]);
+  const [vehicles, setVehicles] = useState<Vehicle[] | null>(null);
 
   async function fetchVehicles() {
     const vehicles = await getStock();
@@ -15,6 +16,18 @@ const VehicleList: React.FC = () => {
   useEffect(() => {
     fetchVehicles();
   }, []);
+
+  if (!vehicles) {
+    return (
+      <Spinner animation="border" role="status">
+        <span className="sr-only">Loading...</span>
+      </Spinner>
+    );
+  }
+
+  if (!vehicles.length) {
+    return <div>Estamos sin stock. Contactanos al 0800-666-123</div>;
+  }
 
   return (
     <ul className="list-unstyled">
