627442648fcd349bf721db9c256e013f5c37e8cb
Matias Chomicki <matias.chomicki@vividcortex.com>
2020-04-24 20:47:24 -0300

    [proxy] configurar proxy al mock de la api
    De esta forma, asumiendo que la API está en el mismo dominio, podemos hacer proxy de la request sin tener que hardcodear el puerto y el dominio.

--- src/services/stock.ts
+++ src/services/stock.ts
@@ -1,15 +1,11 @@
 import axios from "axios";
 import Vehicle, { VehicleData } from "models/Vehicle";
 
-const apiUrl =
-  process.env.NODE_ENV !== "production" ? "localhost:3100" : "stock.com";
 const apiVersion = "v1";
 
 export async function get() {
   try {
-    const { data } = await axios.get(
-      `http://${apiUrl}/api/${apiVersion}/stock`
-    );
+    const { data } = await axios.get(`/api/${apiVersion}/stock`);
 
     return data.data.map((data: VehicleData) => new Vehicle(data));
   } catch (e) {
