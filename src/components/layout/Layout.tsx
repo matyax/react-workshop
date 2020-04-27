9261164f320b6a769f88f5daf9f716faecd2ab25
Matias Chomicki <matias.chomicki@vividcortex.com>
2020-04-27 19:27:12 -0300

    [layout] agregar columna faltante

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,13 +1,15 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import React from "react";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Layout: React.FC = ({ children }) => (
   <>
     <Header />
     <Container fluid>
-      <Row>{children}</Row>
+      <Row>
+        <Col>{children}</Col>
+      </Row>
     </Container>
     <Footer />
   </>
